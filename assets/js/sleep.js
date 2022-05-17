62bf679c0a95c654fb9585eb9a289066da767c82
songfawei <3161488636@qq.com>
2022-05-17 11:19:48 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/sleep.js
@@ -0,0 +1,10 @@
+function sleep(n) {
+    var start = new Date().getTime();
+    //  console.log('休眠前：' + start);
+    while (true) {
+        if (new Date().getTime() - start > n) {
+            break;
+        }
+    }
+    // console.log('休眠后：' + new Date().getTime());
+}
